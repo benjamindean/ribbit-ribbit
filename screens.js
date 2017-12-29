f41583abaffc92b1a4bd508efce18e181d42c20a
Benjamin Dean <berman.reshetnikov@gmail.com>
2017-12-29 16:38:36 +0200

    Cleanup old navigation

--- screens.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import { Navigation } from 'react-native-navigation';
-import CustomPattern from './Pages/Custom';
-
-console.log(CustomPattern);
-
-export function registerScreens(store, Provider) {
-	Navigation.registerComponent('ribbitribbit.CustomPattern', () => CustomPattern, store, Provider);
-}
