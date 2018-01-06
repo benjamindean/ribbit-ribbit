4cb696dfc80939e280a8a6402d4c9815d7aa8cce
Benjamin Dean <berman.reshetnikov@gmail.com>
2018-01-06 18:20:55 +0200

    Refactor Pattern Reducer to Class

--- Reducers/Pattern.js
+++ Reducers/Pattern.js
@@ -1,39 +1,82 @@
-import { ToastAndroid, Vibration } from 'react-native';
-import { PLAY_PATTERN, SET_PATTERN, UPDATE_PATTERN, RESET_PATTERN } from '../Actions/Pattern';
+import {
+	ToastAndroid,
+	Vibration
+} from 'react-native';
+import {
+	PLAY_PATTERN,
+	SET_PATTERN,
+	UPDATE_PATTERN,
+	RESET_PATTERN
+} from '../Actions/Pattern';
 
-function play(state, pattern) {
-	if (!pattern.length) {
-		return state;
+class PatternReducer {
+	constructor(state) {
+		this.state = state;
 	}
 
-	Vibration.vibrate(pattern);
-	ToastAndroid.show(`Playing pattern: ${JSON.stringify(pattern)}`, ToastAndroid.LONG);
-
-	return state;
-}
-
-function reset() {
-	if (!pattern.length) {
-		return state;
+	get value() {
+		return this.state;
 	}
 
-	Vibration.cancel();
-	ToastAndroid.show('Pattern cleared', ToastAndroid.SHORT);
+	set value(pattern) {
+		return this.state = pattern;
+	}
 
-	return [];
+	validate() {
+		if (!Array.isArray(this.state)) {
+			ToastAndroid.show('Invalid vibration pattern', ToastAndroid.SHORT);
+			return false;
+		}
+
+		if (!this.state.length) {
+			return false;
+		}
+
+		return this.state.filter(element => {
+			return Number.isInteger(element);
+		});
+	}
+
+	update(element) {
+		return [...this.state, element];
+	}
+
+	play() {
+		const validPattern = this.validate(this.state);
+
+		if (validPattern) {
+			Vibration.vibrate(validPattern);
+			ToastAndroid.show(`Playing pattern: ${JSON.stringify(validPattern)}`, ToastAndroid.LONG);
+		}
+
+		return this.state;
+	}
+
+	reset() {
+		if (!this.state.length) {
+			return this.state;
+		}
+
+		Vibration.cancel();
+		ToastAndroid.show('Pattern cleared', ToastAndroid.SHORT);
+
+		return [];
+	}
 }
 
 export default function pattern(state = [], action) {
+	const pattern = new PatternReducer(state);
+
 	switch (action.type) {
 		case PLAY_PATTERN:
-			return play(state, action.pattern);
+			return pattern.play();
 		case SET_PATTERN:
-			return action.pattern;
+			return pattern.value(action.patern);
 		case UPDATE_PATTERN:
-			return [...state, action.element];
+			return pattern.update(action.element);
 		case RESET_PATTERN:
-			return reset();
+			return pattern.reset();
 		default:
-			return state;
+			return pattern.value;
 	}
 }
